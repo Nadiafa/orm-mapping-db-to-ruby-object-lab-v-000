7f7dd4f35e5c60b16b7ace287debb03c71805afd
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-15 15:54:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,14 +70,14 @@ class Student
   def self.first_student_in_grade_10
       # returns the first student in grade 10
       sql = <<-SQL
-      SELECT *
+      SELECT FIRST
         FROM students
         WHERE grade = 10
         LIMIT 1
       SQL
     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end 
   # def self.all_students_in_grade_X
   #     # returns an array of all students in a given grade X
