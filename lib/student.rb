5c039236dd9142e48113fe0ec88fdad6c4f68c23
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-15 15:48:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,9 +35,11 @@ class Student
     sql = <<-SQL
       SELECT *
         FROM students
-        WHERE grade = "9"
+        WHERE grade = 9
       SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
